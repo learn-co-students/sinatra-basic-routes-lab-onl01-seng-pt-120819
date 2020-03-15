cecc1a8259dbe3fd9238ef84703eaa17168c35e8
SMakaiTakori <makait87@icloud.com>
2020-03-14 22:14:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Makai" 
+    end
+
+    get '/hometown' do
+        "My hometown is in New York" 
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Love on the Brain" 
+    end
+
 end
