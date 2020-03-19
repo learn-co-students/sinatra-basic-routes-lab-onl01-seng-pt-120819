331c40099c1f87ef9ff7c26ea48bddef651533dc
asif-kamal <asif.kamal1489@gmail.com>
2020-03-19 17:52:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is __"
+    # some code to render the correct HTML page
+  end
+  
+  get '/hometown' do
+    "My hometown is __"
+    # some code to render the correct HTML page
+  end
+
+  get '/favorite-song' do
+    "My favorite song is __"
+    # some code to render the correct HTML page
+  end
+
 end
