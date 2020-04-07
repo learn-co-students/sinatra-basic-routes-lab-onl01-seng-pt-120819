a8be804b84c421841e7a49943fc625542d23f4bc
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-07 04:18:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+        "Hello"
+    end 
+    get "/name" do 
+        "My name is Eryk!"
+    end
+
+    get "/hometown" do 
+        "My hometown is Garwood!"
+    end
+
+    get "/favorite-song" do 
+        "My favorite song is AJ Tracey - 3am"
+    end 
 end
